ca01b85edee0089d74a56c9f9c69bb6c8fd6c32f
DoniNguyen <mew3sg@gmail.com>
2019-10-17 22:50:25 -0500

    update

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,126 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(human){
+        this.name = human.name;
+        this.age = human.age;
+        this.location = human.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`;
+    };
+};
+
+class Instructor extends Person{
+    constructor(inst){
+        super(inst);
+        this.specialty = inst.specialty;
+        this.favLanguage = inst.favLanguage;
+        this.catchPhrase = inst.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    };
+    grade(Student,i){
+        console.log(`${Student.name} recieves a perfect score from ${this.name} in ${Student.favSubjects[i]}`);
+    };
+}
+
+class Student extends Person{
+    constructor(stu){
+        super(stu);
+        this.previousBackground = stu.previousBackground;
+        this.className = stu.previousBackground;
+        this.favSubjects = stu.favSubjects;
+    }
+    listsSubjects = function(){
+        console.log(this.favSubjects);
+    };
+    PRAssignment(subject){
+        console.log(`${this.name} has submitted a PR for ${subject}`);
+    };
+    sprintChallenge(subject){
+        console.log(`${this.name} has begun sprint challenge on ${subject}`);
+    };
+};
+
+class ProjectManager extends Instructor{
+    constructor(proj){
+        super(proj);
+        this.gradClassName = proj.gradClassName;
+        this.favInstructor = proj.favInstructor;
+    };
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+    };
+    debugsCode(Student, subject){
+        console.log(`${this.name} debugs ${Student.name}'s code on ${subject}`);
+    }
+}
+
+const crocker = new Instructor({
+    name: 'crocker',
+    location: 'fairyland',
+    age: '40',
+    favLanguage: 'fairy language',
+    specialty: 'catching fairys',
+    catchPhrase: 'Admit it! You have FAIRY GOD PARENTS!!'
+});
+
+const kimboSlice = new Instructor({
+    name: 'Kimbo-Slice',
+    location: 'The backyard',
+    age: 'Immortal',
+    favLanguage: 'Jive',
+    specialty: 'Destroying',
+    catchPhrase: `C'MON GET UP GET UP`
+});
+
+const dannyP = new Student({
+    name: `Danny Phantom`,
+    previousBackground: `Ghost-Fighter`,
+    className: `Gh0sT-1`,
+    favSubjects: [`Going Ghost`,`Biology`,`Paranormal-studies`]
+});
+
+const spongeBob = new Student({
+    name: `Sponge-Bob Squarepants`,
+    previousBackground: `Fry-Cook`,
+    className: `boating-29`,
+    favSubjects: [`boating`,`jelly-fishing`,`patty-flipping`]
+});
+
+const gohan = new Student({
+    name: `Gohan`,
+    previousBackground: `Z-Fighters`,
+    className: `ki-1342`,
+    favSubjects: [`medicine`,`super101`,`spirtuality`]
+});
+
+const jimmyN = new ProjectManager({
+    name: `Jimmy Neutron`,
+    gradClassName: `sci9000`,
+    favInstructor: `himself`
+});
+
+const robin = new ProjectManager({
+    name: `Robin`,
+    gradClassName: `bat1-1`,
+    favInstructor: `batman`
+});
+
+kimboSlice.grade(gohan,1);
+crocker.demo('catching fairys');
+console.log(crocker.catchPhrase);
+console.log(gohan.previousBackground);
+spongeBob.listsSubjects();
+dannyP.PRAssignment(`ghost101`);
+spongeBob.sprintChallenge(`boating`);
+console.log(jimmyN.gradClassName);
+console.log(robin.favInstructor);
+jimmyN.debugsCode(gohan,`Ki-00`);
+robin.standUp(`bats101`);
+
+
+
+
+
--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -1,9 +1,155 @@
-/* 
 
-Prototype Refactor
 
-1. Copy and paste your code or the solution from yesterday
+// Prototype Refactor
 
-2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+// 1. Copy and paste your code or the solution from yesterday
+// let GameObject = function(game){
+//   this.createdAt = '10/16/2019';
+//   this.dimensions = game.dimensions
+//   }
+//   GameObject.prototype.destroy = function(){
+//     return `${this.name} was removed from the game.`;
+//   };
 
-*/
+class GameObject{
+    constructor(game){
+        this.createdAt = '10/17/2019';
+        this.dimensions = game.dimensions;
+    };
+    destroy(){
+        return `${this.name} was removed from the game.`
+    };
+}
+//   === CharacterStats ===
+//   * healthPoints
+//   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+//   * should inherit destroy() from GameObject's prototype
+// // */
+// let CharacterStats = function(char){
+//     this.healthPoints = char.healthPoints;
+//     GameObject.call(this, char)
+//     CharacterStats.prototype.takeDamage = function(){
+//         return `${this.name} took damage.`;
+//   };  
+//   };
+
+  class CharacterStats extends GameObject{
+      constructor(char){
+          super(char);
+          this.healthPoints = char.healthPoints;
+      }
+      takeDamage(){
+          return `${this.name} took damage.`;
+      };
+  }
+  
+//   CharacterStats.prototype = Object.create(GameObject.prototype);
+//   /*
+//     === Humanoid (Having an appearance or character resembling that of a human.) ===
+//     * team
+//     * weapons
+//     * language
+//     * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+//     * should inherit destroy() from GameObject through CharacterStats
+//     * should inherit takeDamage() from CharacterStats
+// // //   */
+//    let Humanoid = function(human){
+//      this.name = human.name;
+//      this.team = human.team;
+//      this.weapons = human.weapons;
+//      this.language = human.language;
+//      CharacterStats.call(this, human)
+//      Humanoid.prototype.greet = function(){
+//      return `${this.name} offers a greeting in ${this.language}`;
+//    };
+//    };
+
+   class Humanoid extends CharacterStats{
+       constructor(human){
+           super(human)
+           this.name = human.name;
+           this.team = human.team;
+           this.weapons = human.weapons;
+           this.language = human.language;
+       }
+       greet(){
+           return `${this.name} offers a greeting in ${this.language}`;
+       }
+   }
+   
+   Humanoid.prototype = Object.create(CharacterStats.prototype);
+   
+  /*
+    * Inheritance chain: GameObject -> CharacterStats -> Humanoid
+    * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
+    * Instances of CharacterStats should have all of the same properties as GameObject.
+  */
+  // 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
+
+  // Test you work by un-commenting these 3 objects and the list of console logs below:
+  
+  
+    const mage = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 1,
+        height: 1,
+      },
+      healthPoints: 5,
+      name: 'Bruce',
+      team: 'Mage Guild',
+      weapons: [
+        'Staff of Shamalama',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const swordsman = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 2,
+        width: 2,
+        height: 2,
+      },
+      healthPoints: 15,
+      name: 'Sir Mustachio',
+      team: 'The Round Table',
+      weapons: [
+        'Giant Sword',
+        'Shield',
+      ],
+      language: 'Common Tongue',
+    });
+  
+    const archer = new Humanoid({
+      createdAt: new Date(),
+      dimensions: {
+        length: 1,
+        width: 2,
+        height: 4,
+      },
+      healthPoints: 10,
+      name: 'Lilith',
+      team: 'Forest Kingdom',
+      weapons: [
+        'Bow',
+        'Dagger',
+      ],
+      language: 'Elvish',
+    });
+
+
+  console.log(mage.createdAt); // Today's date
+  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+  console.log(swordsman.healthPoints); // 15
+  console.log(mage.name); // Bruce
+  console.log(swordsman.team); // The Round Table
+  console.log(mage.weapons); // Staff of Shamalama
+  console.log(archer.language); // Elvish
+  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+
+
+// */
